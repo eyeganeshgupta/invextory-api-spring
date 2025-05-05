c2b45a6a1adda82dc42f1ae6b578dd3fda3c1d91
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-05 23:02:38 +0530

    setting up development environment

---
+++ src/main/java/com/invextory/InvextoryApiSpringApplication.java
@@ -0,0 +1,11 @@
+package com.invextory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvextoryApiSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InvextoryApiSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/invextory/InvextoryApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invextory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvextoryApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
