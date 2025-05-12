a53a59695f0affc38ec5fd00295309e43498aeb2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-12 21:36:50 +0530

    CategoryDTO

---
+++ src/main/java/com/invextory/dtos/CategoryDTO.java
@@ -0,0 +1,26 @@
+package com.invextory.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.invextory.constants.AppText;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CategoryDTO {
+
+    private Long id;
+
+    @NotBlank(message = AppText.CATEGORY_NAME_REQUIRED)
+    @Size(max = 100, message = AppText.CATEGORY_NAME_TOO_LONG)
+    private String name;
+
+    private List<ProductDTO> products;
+
+}
