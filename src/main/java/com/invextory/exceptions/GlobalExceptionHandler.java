f20b956b148d3bd509eac8a0ab61f49408aeba92
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-16 23:34:07 +0530

    global exception handler

---
+++ src/main/java/com/invextory/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.invextory.exceptions;
+
+import com.invextory.dtos.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response> handleNotFoundException(NotFoundException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NameValueRequiredException.class)
+    public ResponseEntity<Response> handleNameValueRequiredException(NameValueRequiredException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<Response> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleGenericException(Exception ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
