17e72671454c231be68a3ca1adda0e6cd833ec66
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-17 23:13:59 +0530

    CustomAuthenticationEntryPoint

---
+++ src/main/java/com/invextory/exceptions/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.invextory.exceptions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.invextory.dtos.response.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        Response errorResponse = Response.builder().status(HttpStatus.UNAUTHORIZED.value()).message(authException.getMessage()).build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
