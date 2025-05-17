c5dd522bb157173de471ba3565660db3b779b252
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-17 23:19:21 +0530

    CustomAccessDeniedHandler

---
+++ src/main/java/com/invextory/exceptions/CustomAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.invextory.exceptions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.invextory.dtos.response.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper mapper;
+
+    public CustomAccessDeniedHandler(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        Response errorResponse = Response.builder().status(HttpStatus.FORBIDDEN.value()).message(accessDeniedException.getMessage()).build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().write(mapper.writeValueAsString(errorResponse));
+    }
+}
