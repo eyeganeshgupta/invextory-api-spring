2e4a9863072fbf6af83cc81d8312eb9f1f936f17
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-18 18:20:33 +0530

    CustomUserDetailsService

---
+++ src/main/java/com/invextory/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.invextory.security;
+
+import com.invextory.exceptions.NotFoundException;
+import com.invextory.models.User;
+import com.invextory.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new NotFoundException("No user found with the provided email address."));
+
+        return AuthUser.builder().user(user).build();
+
+    }
+}
