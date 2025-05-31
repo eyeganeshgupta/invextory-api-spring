6ce807a93dfb226e45f5d04ccad85c98ccff1466
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-31 19:40:14 +0530

    user controller

---
+++ src/main/java/com/invextory/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.invextory.controllers;
+
+import com.invextory.dtos.UserDTO;
+import com.invextory.dtos.response.Response;
+import com.invextory.models.User;
+import com.invextory.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Response> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.deleteUser(id));
+    }
+
+    @GetMapping("/transactions/{userId}")
+    public ResponseEntity<Response> getUserAndTransactions(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getUserTransactions(userId));
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<User> getCurrentUser() {
+        return ResponseEntity.ok(userService.getCurrentLoggedInUser());
+    }
+
+}
