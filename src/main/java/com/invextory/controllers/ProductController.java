9684580a1286055d511443827c7dbd98aab74d6a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-06-19 20:03:13 +0530

    product controller

--- src/main/java/com/invextory/controllers/ProductController.java
+++ src/main/java/com/invextory/controllers/ProductController.java
@@ -1,8 +1,11 @@
 package com.invextory.controllers;
 
+import com.invextory.dtos.ProductDTO;
 import com.invextory.dtos.response.Response;
 import com.invextory.services.ProductService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,6 +18,13 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> createProduct(@RequestBody @Valid ProductDTO productDTO) {
+        Response response = productService.createProduct(productDTO);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
     public ResponseEntity<Response> getAllProducts() {
@@ -26,6 +36,22 @@ public class ProductController {
     @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
     public ResponseEntity<Response> getProductById(@PathVariable @Min(1) Long id) {
         Response response = productService.getProductById(id);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> updateProduct(
+            @PathVariable @NotNull Long id,
+            @RequestBody @Valid ProductDTO productDTO) {
+        Response response = productService.updateProduct(id, productDTO);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> deleteProduct(@PathVariable Long id) {
+        Response response = productService.deleteProduct(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
